b0701c56cbf5a54d747ae8c50590f494bc6f00f9
Nitya <nityapandey2004@gmail.com>
2026-02-24 11:08:31 +0530

    [Nitya]: completed UC2

---
+++ src/test/java/com/example/QuantityMeasurementAppTest.java
@@ -0,0 +1,51 @@
+package com.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.QuantityMeasurementApp.Feet;
+import com.example.QuantityMeasurementApp.Inches;;
+
+public class QuantityMeasurementAppTest {
+
+	@Test
+	public void testFeetEquality_SameValue() {
+		Feet f1 = new Feet(1.0);
+		Feet f2 = new Feet(1.0);
+		assertTrue(f1.equals(f2));
+	}
+
+	@Test
+	public void testFeetEquality_DifferentValue() {
+		Feet f1 = new Feet(1.0);
+		Feet f2 = new Feet(2.0);
+		assertFalse(f1.equals(f2));
+	}
+
+	@Test
+	public void testFeetEquality_NullComparison() {
+		Feet f1 = new Feet(1.0);
+		assertFalse(f1.equals(null));
+	}
+
+	@Test
+	public void testFeetEquality_DifferentClass() {
+		Feet f1 = new Feet(1.0);
+		String other = "NotFeet";
+		assertFalse(f1.equals(other));
+	}
+
+	@Test
+	public void testFeetEquality_SameReference() {
+		Feet f1 = new Feet(1.0);
+		assertTrue(f1.equals(f1));
+	}
+
+	// testing for Feet and Inches Equality
+
+	@Test
+	public void testInchesEquality_SameValue() {
+		Inches i1 = new Inches(1.0);
+		Inches i2 = new Inches(1.0);
+		assertTrue(i1.equals(i2));
+	}
+}
